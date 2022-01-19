3a6167532ec4c992a2ff6bcd9c65b45bab049cd3
nyaafree <45682975+nyaafree@users.noreply.github.com>
2022-01-19 10:22:44 +0900

    todoフォームに入力した内容を未完了リストに追加

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import "./styles.css";
 
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化
+  const inputText = document.getElementById("add-text").value;
+  console.log(inputText);
+  document.getElementById("add-text").value = "";
+
+  // divタグ生成
+  const todoDiv = document.createElement("div");
+  console.log(todoDiv);
+  todoDiv.className = "list-row";
+
+  // liタグ生成
+  const todoLi = document.createElement("li");
+  todoLi.innerText = inputText;
+  console.log(todoLi);
+
+  // divの下にliを入れる
+  todoDiv.appendChild(todoLi);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(todoDiv);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
